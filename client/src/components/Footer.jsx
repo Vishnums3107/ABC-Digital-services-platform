aa700ff3688889f9a15ca30012baef8cafeef7d5
Vishnums3107 <msvishnu3107@gmail.com>
2025-08-27 08:34:15 +0530

    last

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer style={{ backgroundColor: "#222", color: "white", padding: "2rem", textAlign: "center" }}>
       <p>&copy; 2023 ABC Corporate Solutions. All rights reserved.</p>
-      <p>Online Thamizhan</p>
+      <p>Best Solutions for Your Business</p>
     </footer>
   );
 }
